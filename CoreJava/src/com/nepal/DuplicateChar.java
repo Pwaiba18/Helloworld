de735fa915fa4ea58a288c63fc679a8819f0238d
Prerna Waiba <waiba.prerna@gmail.com>
2023-06-11 22:51:46 -0400

    This is my first commit

--- CoreJava/src/com/nepal/DuplicateChar.java
+++ CoreJava/src/com/nepal/DuplicateChar.java
@@ -3,13 +3,15 @@ package com.nepal;
 public class DuplicateChar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		String str = "maheshbabu";
 				int length = str.length();
 				char[] ch = str.toCharArray();
-				for (int i = 0; i< length; i++) {
+				for (int i = 0; i< length; i++)
+				{
 					for (int j = i + 1; j < length; j++) {
-						if (ch[i] == ch [j]) {
+						if (ch[i] == ch [j])
+						{
 							System.out.println("Duplicate characters are:  " +ch[j]);
 							break;
 						}
